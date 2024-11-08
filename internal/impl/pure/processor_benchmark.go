7b4b1917faccd4b128bd2795439a4730289aba87
Wesley Merkel <wesley.merkel@redpanda.com>
2024-11-08 11:19:26 -0700

    feat(benchmark): allow skipping byte throughput measurement
    This helps users avoid taking an unnecessary performance hit in the case
    that they have a pipeline that does not require serializing the message.

--- internal/impl/pure/processor_benchmark.go
+++ internal/impl/pure/processor_benchmark.go
@@ -19,7 +19,8 @@ func init() {
 }
 
 const (
-	bmFieldInterval = "interval"
+	bmFieldInterval   = "interval"
+	bmFieldCountBytes = "count_bytes"
 )
 
 func benchmarkSpec() *service.ConfigSpec {
@@ -29,8 +30,11 @@ func benchmarkSpec() *service.ConfigSpec {
 		Description("Logs messages per second and bytes per second of messages that are processed at a regular interval. A summary of the amount of messages processed over the entire lifetime of the processor will also be printed when the processor shuts down.").
 		Field(service.NewDurationField(bmFieldInterval).
 			Description("How often to emit rolling statistics. If set to 0, only a summary will be logged when the processor shuts down.").
-			Default("5s").
-			Description("How often to emit rolling statistics."),
+			Default("5s"),
+		).
+		Field(service.NewBoolField(bmFieldCountBytes).
+			Description("Whether or not to measure the number of bytes per second of throughput. Counting the number of bytes requires serializing structured data, which can cause an unnecessary performance hit if serialization is not required elsewhere in the pipeline.").
+			Default(true),
 		)
 }
 
@@ -39,10 +43,15 @@ func newBenchmarkProcFromConfig(conf *service.ParsedConfig, mgr *service.Resourc
 	if err != nil {
 		return nil, err
 	}
+	countBytes, err := conf.FieldBool(bmFieldCountBytes)
+	if err != nil {
+		return nil, err
+	}
 
 	b := &benchmarkProc{
 		startTime:       time.Now(),
 		rollingInterval: interval,
+		countBytes:      countBytes,
 		logger:          mgr.Logger(),
 	}
 
@@ -60,6 +69,7 @@ func newBenchmarkProcFromConfig(conf *service.ParsedConfig, mgr *service.Resourc
 type benchmarkProc struct {
 	startTime       time.Time
 	rollingInterval time.Duration
+	countBytes      bool
 	logger          *service.Logger
 
 	lock         sync.Mutex
@@ -71,16 +81,22 @@ type benchmarkProc struct {
 }
 
 func (b *benchmarkProc) Process(ctx context.Context, msg *service.Message) (service.MessageBatch, error) {
-	bytes, err := msg.AsBytes()
-	if err != nil {
-		return nil, fmt.Errorf("getting message bytes: %w", err)
+	var bytesCount float64
+	if b.countBytes {
+		bytes, err := msg.AsBytes()
+		if err != nil {
+			return nil, fmt.Errorf("getting message bytes: %w", err)
+		}
+		bytesCount = float64(len(bytes))
 	}
 
-	bytesCount := float64(len(bytes))
-
 	b.lock.Lock()
-	b.rollingStats.recordMessage(bytesCount)
-	b.totalStats.recordMessage(bytesCount)
+	b.rollingStats.msgCount++
+	b.totalStats.msgCount++
+	if b.countBytes {
+		b.rollingStats.msgBytesCount += bytesCount
+		b.totalStats.msgBytesCount += bytesCount
+	}
 	b.lock.Unlock()
 
 	return service.MessageBatch{msg}, nil
@@ -116,27 +132,32 @@ func (b *benchmarkProc) sampleRolling() benchmarkStats {
 func (b *benchmarkProc) printStats(window string, stats benchmarkStats, interval time.Duration) {
 	secs := interval.Seconds()
 	msgsPerSec := stats.msgCount / secs
-	bytesPerSec := stats.msgBytesCount / secs
-	b.logger.
-		With(
-			"msg/sec", msgsPerSec,
-			"bytes/sec", bytesPerSec,
-		).
-		Infof(
-			"%s stats: %s msg/sec, %s/sec",
-			window,
-			humanize.Ftoa(msgsPerSec),
-			humanize.Bytes(uint64(bytesPerSec)),
-		)
+
+	if b.countBytes {
+		bytesPerSec := stats.msgBytesCount / secs
+		b.logger.
+			With(
+				"msg/sec", msgsPerSec,
+				"bytes/sec", bytesPerSec,
+			).
+			Infof(
+				"%s stats: %s msg/sec, %s/sec",
+				window,
+				humanize.Ftoa(msgsPerSec),
+				humanize.Bytes(uint64(bytesPerSec)),
+			)
+	} else {
+		b.logger.
+			With("msg/sec", msgsPerSec).
+			Infof(
+				"%s stats: %s msg/sec",
+				window,
+				humanize.Ftoa(msgsPerSec),
+			)
+	}
 }
 
 type benchmarkStats struct {
 	msgCount      float64
 	msgBytesCount float64
-}
-
-func (s *benchmarkStats) recordMessage(bytesCount float64) {
-	s.msgCount++
-	s.msgCount++
-	s.msgBytesCount += float64(bytesCount)
 }
