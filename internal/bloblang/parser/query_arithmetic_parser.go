96f7b5c05c8a31a389394526af4a9483a2c02d55
eadwright <eadwright@gmail.com>
2025-05-07 16:43:53 +0100

    Pivot to using methods not math operators, add AND and OR

--- internal/bloblang/parser/query_arithmetic_parser.go
+++ internal/bloblang/parser/query_arithmetic_parser.go
@@ -23,7 +23,6 @@ var arithmeticOpPattern = OneOf(
 	Char('>'),
 	Char('<'),
 	Char('|'),
-	Char('^'),
 )
 
 func arithmeticOpParser(input []rune) Result[query.ArithmeticOperator] {
@@ -62,8 +61,6 @@ func arithmeticOpParser(input []rune) Result[query.ArithmeticOperator] {
 		outRes.Payload = query.ArithmeticLte
 	case "|":
 		outRes.Payload = query.ArithmeticPipe
-	case "^":
-		outRes.Payload = query.ArithmeticXor
 	default:
 		return Fail[query.ArithmeticOperator](
 			NewFatalError(input, fmt.Errorf("operator not recognized: %v", res.Payload)),
