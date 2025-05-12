30378262cdef4f9a2bd7b1325d2a9b70c2918a71
Mihai Todor <mihai.todor@contractor.redpanda.com>
2025-05-12 13:54:12 +0100

    Fix broken test
    Thank you for reporting this @dcarbone!
    
    Co-authored-by: Daniel Carbone <daniel.p.carbone@gmail.com>
    Signed-off-by: Mihai Todor <mihai.todor@contractor.redpanda.com>

--- public/service/stream_builder_test/stream_builder_test.go
+++ public/service/stream_builder_test/stream_builder_test.go
@@ -157,8 +157,16 @@ foobar: {}
 
 	closeChan := make(chan struct{})
 	go func() {
-		require.NoError(t, stream.Run(t.Context()))
+		err := stream.Run(t.Context())
+		require.NoError(t, err)
+
 		close(closeChan)
+	}()
+	defer func() {
+		err := stream.StopWithin(1 * time.Second)
+		require.NoError(t, err)
+
+		<-closeChan
 	}()
 
 	select {
