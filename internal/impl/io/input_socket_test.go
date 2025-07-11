0d3a34044c20fd7b02d0a6b7f5bc546252aa5664
eadwright <eadwright@gmail.com>
2025-07-11 09:52:16 +0100

    Add TLS unit test for input socket

--- internal/impl/io/input_socket_test.go
+++ internal/impl/io/input_socket_test.go
@@ -5,8 +5,15 @@ package io
 import (
 	"bufio"
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
 	"errors"
 	"fmt"
+	"math/big"
 	"net"
 	"path/filepath"
 	"reflect"
@@ -986,6 +993,135 @@ socket:
 	}
 
 	wg.Wait()
+}
+
+func TestTCPSocketInputTLS(t *testing.T) {
+	ctx, done := context.WithTimeout(t.Context(), time.Second*20)
+	defer done()
+
+	tlsConfig, err := generateSelfSignedTLSConfig()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ln, err := tls.Listen("tcp", "127.0.0.1:0", tlsConfig)
+	if err != nil {
+		if ln, err = net.Listen("tcp6", "[::1]:0"); err != nil {
+			t.Fatalf("failed to listen on a port: %v", err)
+		}
+	}
+	defer ln.Close()
+
+	rdr := inputFromConf(t, `
+socket:
+  network: tcp
+  address: %v
+  tls:
+    enabled: true
+    skip_cert_verify: true
+`, ln.Addr().String())
+
+	defer func() {
+		rdr.TriggerStopConsuming()
+		if err := rdr.WaitForClose(ctx); err != nil {
+			t.Error(err)
+		}
+	}()
+
+	conn, err := ln.Accept()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		_ = conn.SetWriteDeadline(time.Now().Add(time.Second * 5))
+		if _, cerr := conn.Write([]byte("foo\n")); cerr != nil {
+			t.Error(cerr)
+		}
+		if _, cerr := conn.Write([]byte("bar\n")); cerr != nil {
+			t.Error(cerr)
+		}
+		if _, cerr := conn.Write([]byte("baz\n")); cerr != nil {
+			t.Error(cerr)
+		}
+		wg.Done()
+	}()
+
+	readNextMsg := func() (message.Batch, error) {
+		var msg message.Batch
+		select {
+		case tran := <-rdr.TransactionChan():
+			msg = tran.Payload.DeepCopy()
+			require.NoError(t, tran.Ack(ctx, nil))
+		case <-time.After(time.Second):
+			return nil, errors.New("timed out")
+		}
+		return msg, nil
+	}
+
+	exp := [][]byte{[]byte("foo")}
+	msg, err := readNextMsg()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if act := message.GetAllBytes(msg); !reflect.DeepEqual(exp, act) {
+		t.Errorf("Wrong message contents: %s != %s", act, exp)
+	}
+
+	exp = [][]byte{[]byte("bar")}
+	if msg, err = readNextMsg(); err != nil {
+		t.Fatal(err)
+	}
+	if act := message.GetAllBytes(msg); !reflect.DeepEqual(exp, act) {
+		t.Errorf("Wrong message contents: %s != %s", act, exp)
+	}
+
+	exp = [][]byte{[]byte("baz")}
+	if msg, err = readNextMsg(); err != nil {
+		t.Fatal(err)
+	}
+	if act := message.GetAllBytes(msg); !reflect.DeepEqual(exp, act) {
+		t.Errorf("Wrong message contents: %s != %s", act, exp)
+	}
+
+	wg.Wait()
+	conn.Close()
+}
+
+// Generate a self-signed TLS config (in-memory)
+func generateSelfSignedTLSConfig() (*tls.Config, error) {
+	// Generate key
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create certificate
+	template := &x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject:      pkix.Name{CommonName: "localhost"},
+		NotBefore:    time.Now().Add(-time.Hour),
+		NotAfter:     time.Now().Add(time.Hour),
+		KeyUsage:     x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+	}
+
+	certDER, err := x509.CreateCertificate(rand.Reader, template, template, &key.PublicKey, key)
+	if err != nil {
+		return nil, err
+	}
+
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	cert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		return nil, err
+	}
+
+	return &tls.Config{Certificates: []tls.Certificate{cert}}, nil
 }
 
 func BenchmarkTCPSocketWithCutOff(b *testing.B) {
