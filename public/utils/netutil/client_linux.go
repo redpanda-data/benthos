76395885287e7cb01abd36001fcbced759e249b7
Michał Matczuk <michal.matczuk@redpanda.com>
2025-10-17 11:41:01 +0200

    utils/netutil: ingest netclient

--- public/utils/netutil/client_linux.go
+++ public/utils/netutil/client_linux.go
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package netclient
+package netutil
 
 import (
 	"fmt"
--- public/utils/netutil/client_other.go
+++ public/utils/netutil/client_other.go
@@ -13,7 +13,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package netclient
+package netutil
 
 // SetTCPUserTimeout does not apply to non-linux systems as it is not
 // supported on other platforms. Errors and warnings will be silently ignored.
